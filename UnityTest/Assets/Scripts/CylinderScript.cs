063087f94b0aeda8bddc7819e04619e55a516c8b
Tyler Sargent <github@sargent.io>
2016-03-01 15:34:56 -0500

    Added Cylinder prefab

---
+++ UnityTest/Assets/Scripts/CylinderScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CylinderScript : MonoBehaviour {
+
+    [SerializeField]
+    public int aValue;
+    public bool aBool;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.position = new Vector3 (
+            Mathf.PingPong(Time.time*2, 3), Mathf.PingPong(Time.time, 3), transform.position.z);
+    }
+}
