9bd31d03c977c268238433651a90c7d5be4e26ee
Tyler Sargent <github@sargent.io>
2016-03-03 14:44:04 -0500

    Added persistent game object

---
+++ UnityTest/Assets/Scripts/GameControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl control;
+
+    public float health;
+    public float experience;
+
+    // Checks if the control object exists, if not, save it
+    void Awake() {
+        if( control == null){
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        } else if( control != null) {
+            Destroy(gameObject);
+        }
+    }
+}
